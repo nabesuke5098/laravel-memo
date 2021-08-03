3c0347363e32deb4b8360f32abeb372cfbb54b06
nabesuke <nabesuke5098@gmail.com>
2021-08-04 08:35:28 +0900

    [add]create.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,7 +99,6 @@
                     </div>
                 </div>
                 <div class="col-md-6 p-0">
-                    右カラム
                     @yield('content')
                 </div>
             </div>
